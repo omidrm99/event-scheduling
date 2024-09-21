8aaa08a789722a0e4ef0a8d44c629fd238c81bce
omidrm99 <omidrm99@gmail.com>
2024-09-21 21:40:08 +0330

    relations are added to api and now you can get realation by postman

--- database/seeders/AttendeeSeeder.php
+++ database/seeders/AttendeeSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\Attendee;
 use App\Models\Event;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class AttendeeSeeder extends Seeder
